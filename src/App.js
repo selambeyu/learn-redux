bc798fbdf552582baa62b13f91ae3a3e3b5f7e84
melkam beyene <melkam3392beyu@gmail.com>
2019-12-11 20:39:52 -0800

    commit component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 // import logo from './logo.svg';
 import {useSelector, useDispatch} from 'react-redux'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {increment,decrement} from './actions'
+import About from './about'
+import Shop from './shop'
 import './App.css';
 
 function App() {
@@ -10,13 +13,16 @@ function App() {
   const dispatch=useDispatch();
 
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <h1>Counter {counter}</h1>
       {isLogged ?"Valuabel information which i shoudn't see":" "}
       <button onClick={()=>dispatch(increment(5))}>+</button>
       <button onClick={()=>dispatch(decrement(5))}>-</button>
 
     </div>
+    </Router>
+    
   );
 }
 
