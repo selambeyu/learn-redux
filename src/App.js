cf753e02ba1ab071c4703c2b0e40a9620780e230
melkam beyene <melkam3392beyu@gmail.com>
2019-12-11 20:19:39 -0800

    commit updates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 // import logo from './logo.svg';
-import {useSelector} from 'react-redux'
-// import {increment,decrement} from './actions'
+import {useSelector, useDispatch} from 'react-redux'
+import {increment,decrement} from './actions'
 import './App.css';
 
 function App() {
   const counter=useSelector(state=>state.counter);
-  // // const isLogged=useSelector(state=>state.isLogged);
-  // // const dispatch=useDispatch();
+  const isLogged=useSelector(state=>state.isLogged);
+  const dispatch=useDispatch();
 
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
-      <button>+</button>
-      <button>-</button>
+      {isLogged ?"Valuabel information which i shoudn't see":" "}
+      <button onClick={()=>dispatch(increment(5))}>+</button>
+      <button onClick={()=>dispatch(decrement(5))}>-</button>
 
     </div>
   );
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,16 @@
-export const increment=()=>{
+export const increment=(nr)=>{
     return{
-        type:'INCREMENT'
+        type:'INCREMENT',
+        payload: nr
     }
   
 
 }
 
-export const decrement=()=>{
+export const decrement=(nr)=>{
     return{
-        type:'DECREMENT'
+        type:'DECREMENT',
+        payload: nr
     }
 }
 
